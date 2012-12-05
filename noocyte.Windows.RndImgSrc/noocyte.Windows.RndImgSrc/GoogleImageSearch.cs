e5aded9126a208f53985ffdb74aa7b1dfde0e1cd
Jarle Nygård <jarle.nygaard@outlook.com>
2012-12-05 09:23:30 +0100

    Adding class for Google Image Search

---
+++ noocyte.Windows.RndImgSrc/noocyte.Windows.RndImgSrc/GoogleImageSearch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking.Connectivity;
+using Windows.Networking;
+using Windows.Networking.;
+
+namespace noocyte.Windows.RndImgSrc
+{
+    class GoogleImageSearch
+    {
+        public static string CurrentIPAddress()
+        {
+            var icp = NetworkInformation.GetInternetConnectionProfile();
+
+            if (icp != null && icp.NetworkAdapter != null)
+            {
+                var hostname = NetworkInformation.GetHostNames().FirstOrDefault();
+
+                if (hostname != null)
+                {
+                    // the ip address
+                    return hostname.CanonicalName;
+                }
+            }
+
+            return string.Empty;
+        }
+
+        public ItemCollection GetImages(string searchQuery)
+        {
+
+
+            return null;
+        }
+    }
+}
